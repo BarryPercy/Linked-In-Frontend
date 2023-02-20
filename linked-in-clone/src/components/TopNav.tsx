d3cdfa95645d4baca090ab6f982f075108bd8473
Barry Percy <baz.percy@gmail.com>
2023-02-20 13:40:01 +0000

    Changed Navbar styles

--- linked-in-clone/src/components/TopNav.tsx
+++ linked-in-clone/src/components/TopNav.tsx
@@ -1,32 +1,34 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {BsLinkedin} from 'react-icons/bs'
+import { Container,Nav, Navbar, Form, Image, InputGroup } from 'react-bootstrap';
+import { BsLinkedin, BsFillPeopleFill, BsFillBriefcaseFill } from 'react-icons/bs'
+import { AiFillHome, AiFillMessage } from 'react-icons/ai'
+import { RxMagnifyingGlass } from 'react-icons/rx'
+import { FaBell } from 'react-icons/fa'
 const TopNav = ()=>{
     return(
-        <Navbar bg="white" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home"><BsLinkedin/></Navbar.Brand>
+        <Navbar bg="white" expand="lg" fixed="top">
+            <Container className="nav-padding">
+                <div className="d-flex align-items-center">
+                    <Navbar.Brand href="#home"><BsLinkedin size="2em" className='linked-in-nav-icon'/></Navbar.Brand>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2 form-background"
+                            aria-label="Search"
+                        />
+                </Form>
+                </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Nav className="me-auto">
-                    <Nav.Link href="#link">Home</Nav.Link>
-                    <Nav.Link href="#link">My Network</Nav.Link>
-                    <Nav.Link href="#link">Jobs</Nav.Link>
-                    <Nav.Link href="#link">Messaging</Nav.Link>
-                    <Nav.Link href="#link">Notifications</Nav.Link>
-                    <NavDropdown title="Me" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto justify-content-end">
+                        <Nav.Link href="#home"><div className="d-flex flex-column align-items-center nav-options"><AiFillHome size="2em"/><span className="nav-text">Home</span></div></Nav.Link>
+                        <Nav.Link href="#my-net"><div className="d-flex flex-column align-items-center nav-options"><BsFillPeopleFill size="2em"/><span className="nav-text">My Network</span></div></Nav.Link>
+                        <Nav.Link href="#jobs"><div className="d-flex flex-column align-items-center nav-options"><BsFillBriefcaseFill size="2em"/><span className="nav-text">Jobs</span></div></Nav.Link>
+                        <Nav.Link href="#messaging"><div className="d-flex flex-column align-items-center nav-options"><AiFillMessage size="2em"/><span className="nav-text">Messaging</span></div></Nav.Link>
+                        <Nav.Link href="#notif"><div className="d-flex flex-column align-items-center nav-options"><FaBell size="2em"/><span className="nav-text">Notifications</span></div></Nav.Link>
+                        <Nav.Link href=""><div className="d-flex flex-column align-items-center nav-options"><Image src="images/jovelynn.jfif" className="user-image"/>Me</div></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
