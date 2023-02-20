3f2093c11e5cb47d7837daed7add4b97ca2c1727
Barry Percy <baz.percy@gmail.com>
2023-02-20 11:02:21 +0000

    created initial navbar

--- linked-in-clone/src/components/TopNav.tsx
+++ linked-in-clone/src/components/TopNav.tsx
@@ -1,5 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {BsLinkedin} from 'react-icons/bs'
 const TopNav = ()=>{
-
+    return(
+        <Navbar bg="white" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home"><BsLinkedin/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Nav className="me-auto">
+                    <Nav.Link href="#link">Home</Nav.Link>
+                    <Nav.Link href="#link">My Network</Nav.Link>
+                    <Nav.Link href="#link">Jobs</Nav.Link>
+                    <Nav.Link href="#link">Messaging</Nav.Link>
+                    <Nav.Link href="#link">Notifications</Nav.Link>
+                    <NavDropdown title="Me" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default TopNav
