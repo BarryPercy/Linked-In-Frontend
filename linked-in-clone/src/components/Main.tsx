c61ebf6975a60e0a9ccab6ab7f72d645bebda6ab
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-02-20 16:25:45 +0000

    side bar work in progress

--- linked-in-clone/src/components/Main.tsx
+++ linked-in-clone/src/components/Main.tsx
@@ -1,21 +1,34 @@
 import { Container, Row, Col } from "react-bootstrap";
 import About from "./About";
 import Profile from "./Profile";
+import Sidebar from "./Sidebar";
 import TopNav from "./TopNav";
 
 const Main = () => {
   return (
     <>
       <TopNav />
-      <Container>
-        <Row className="mt-3">
-          <Col>
-            <Profile />
-          </Col>
-          <Col>
-            <About />
-          </Col>
-        </Row>
+      <Container className="d-flex">
+        <div className="d-flex flex-column pt-5">
+          {/* left main section */}
+          <Row className="mt-3">
+            <Col sm={6} md={8} className="mt-2 mb-3">
+              <Profile />
+            </Col>
+            <Col sm={6} md={8} className="mt-2 mb-3">
+              <About />
+            </Col>
+          </Row>
+        </div>
+        <div className="d-flex flex-column pt-5">
+          {/* side section */}
+          <Row className="d-flex flex-column mt-3">
+            <Col>
+              <Sidebar />
+            </Col>
+            <Col>Area 2</Col>
+          </Row>
+        </div>
       </Container>
     </>
   );
--- linked-in-clone/src/components/Sidebar.tsx
+++ linked-in-clone/src/components/Sidebar.tsx
@@ -1,5 +1,75 @@
-const Sidebar = ()=>{
+import { Card, Button, Image } from "react-bootstrap";
+import { MdPersonAddAlt1 } from "react-icons/md";
+import { SlArrowDown } from "react-icons/sl";
 
-}
+const Sidebar = () => {
+  return (
+    <div className="sidebar1 ">
+      <h4 className="ml-3 pt-3">People also viewed</h4>
+      <Card>
+        <div className="d-flex mx-3">
+          <Image
+            className="side-pro-icon"
+            roundedCircle
+            src="https://pbs.twimg.com/profile_images/1485050791488483328/UNJ05AV8_400x400.jpg"
+          />
+          <Card.Body className="ml-2">
+            <Card.Title>Rokit Kumar</Card.Title>
+            <Card.Text>
+              Technical Support Enginner at Kontext Security
+            </Card.Text>
+            <Button className="connect-btn" variant="outline-secondary">
+              <MdPersonAddAlt1 className="mr-1" />
+              Connect
+            </Button>
+          </Card.Body>
+        </div>
+        <hr className="line-1" />
+        <div className="d-flex mx-3">
+          <Image
+            className="side-pro-icon"
+            roundedCircle
+            src="https://pbs.twimg.com/profile_images/1485050791488483328/UNJ05AV8_400x400.jpg"
+          />
+          <Card.Body className="ml-2">
+            <Card.Title>Rokit Kumar</Card.Title>
+            <Card.Text>
+              Technical Support Enginner at Kontext Security
+            </Card.Text>
+            <Button className="connect-btn" variant="outline-secondary">
+              <MdPersonAddAlt1 className="mr-1" />
+              Connect
+            </Button>
+          </Card.Body>
+        </div>
+        <hr className="line-1" />
+        <div className="d-flex mx-3">
+          <Image
+            className="side-pro-icon"
+            roundedCircle
+            src="https://pbs.twimg.com/profile_images/1485050791488483328/UNJ05AV8_400x400.jpg"
+          />
+          <Card.Body className="ml-2">
+            <Card.Title>Rokit Kumar</Card.Title>
+            <Card.Text>
+              Technical Support Enginner at Kontext Security
+            </Card.Text>
+            <Button className="connect-btn" variant="outline-secondary">
+              <MdPersonAddAlt1 className="mr-1" />
+              Connect
+            </Button>
+          </Card.Body>
+        </div>
+        <hr className="line-2" />
+        <Button
+          variant="outline-secondary"
+          className="text-center show-btn mt-0"
+        >
+          Show more <SlArrowDown />
+        </Button>
+      </Card>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
