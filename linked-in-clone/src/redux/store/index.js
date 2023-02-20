b4cab9e935f10eb8ad9ee25c9c10f39710b7070b
Barry Percy <baz.percy@gmail.com>
2023-02-20 14:56:28 +0000

    Created reducer structure

---
+++ linked-in-clone/src/redux/store/index.js
@@ -0,0 +1,28 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import usersReducer from '../reducers/usersReducer'
+import mySkillsReducer from '../reducers/mySkillsReducer'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+
+
+const persistConfig = {
+  storage: storage,
+  key: 'root',
+}
+const combinedReducer = combineReducers({
+    mySkills: mySkillsReducer,
+    users: usersReducer,
+})
+const persistedReducer = persistReducer(persistConfig, combinedReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    })
+  },
+})
+const persistedStore = persistStore(store)
+
+export { store, persistedStore }
