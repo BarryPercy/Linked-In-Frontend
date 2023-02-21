85f5225ca7b401a7a1494a4d6e6721a1648930a4
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-02-21 09:32:18 +0000

    experience base layout

--- linked-in-clone/src/components/Experience.tsx
+++ linked-in-clone/src/components/Experience.tsx
@@ -1,5 +1,26 @@
-const Experience = ()=>{
+import { Card } from "react-bootstrap";
+import { BsPencil } from "react-icons/bs";
 
-}
+const Experience = () => {
+  return (
+    <div className="about-section">
+      <Card>
+        <Card.Body>
+          <div className="edit-main mr-4">
+            <BsPencil className="pencil-icon" />
+          </div>
+          <Card.Title>Flight Attendant</Card.Title>
+          <Card.Text>
+            Ryanair - Europe's Favourite Airline · Full-time
+          </Card.Text>
+          <Card.Subtitle className="mb-2 text-muted">
+            Nov 2018 - Nov 2022 · 4 yrs 1 moNov 2018 - Nov 2022 · 4 yrs 1 mo
+            Belgium
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
