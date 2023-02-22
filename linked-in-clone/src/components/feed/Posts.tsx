8c0663841b5ba8e9c47621d1f75bb92ef739b1d6
Barry Percy <baz.percy@gmail.com>
2023-02-22 13:22:40 +0000

    Posts Showing

--- linked-in-clone/src/components/feed/Posts.tsx
+++ linked-in-clone/src/components/feed/Posts.tsx
@@ -2,32 +2,73 @@ import { fetchPosts} from "../../redux/actions";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { useEffect, useState } from "react";
 import { Card, Image, Button } from 'react-bootstrap'
+import parseISO from 'date-fns/parseISO'
+import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict'
 
 interface PostsInterface{
     _id: string;
     text: string;
     username: string;
+    user: User;
     createdAt: string;
     updatedAt: string;
     __v: number;
 }
+
+interface User {
+    _id: string;
+    name: string;
+    surname: string;
+    email: string;
+    bio: string;
+    title: string;
+    area: string;
+    image: string;
+    username: string; 
+    createdAt: string; 
+    updatedAt: string; 
+    __v: number;
+  }
 const Posts = () => {
     const dispatch = useAppDispatch();
     let posts = useAppSelector((state) => state.posts.postList);
+    let postsReverse = [...posts].reverse();
+    let slicedPostsReverse = postsReverse.slice(0,20);
 
+    const formatDate=(date: string)=>{
+        const postDate = parseISO(date)
+        const formatted = formatDistanceToNowStrict(postDate)
+        return(
+            {formatted}
+        )
+    }
     useEffect(() => {
         dispatch(fetchPosts());
       }, []);
     return (
-        <div className="posts">
-            {posts.map((post: PostsInterface) => {
+        <div>
+            {slicedPostsReverse.map((post: PostsInterface) => {
                 return (
-                    <Card key={post._id}>
-                        <Card.Body className="ml-2">
-                            <Card.Title>
-                            </Card.Title>
-                            <Card.Text>{post.text}</Card.Text>
+                    <Card key={post._id} className="posts">
+                        <Card.Body className="ml-2 d-flex">
+                            <Card.Img src={post.user.image} className="avatar align-self-start"/>
+                            <div>
+                                <Card.Title>
+                                    <>
+                                        <h6>{post.user.name} {post.user.surname}</h6>
+                                        <h6 className="post-user-title">{post.user.title}</h6>
+                                        <h6 className="post-user-title">{formatDistanceToNowStrict(parseISO(post.createdAt))} ago</h6>
+                                    </>
+                                </Card.Title>
+                                <Card.Text>
+                                    <>
+                                        <p>{post.text}</p>
+                                        <p></p>
+                                    </>
+                                </Card.Text>
+                            </div> 
                         </Card.Body>
+                        
                     </Card>
                 );
             })}
