c5432c55b4448c1823c6b69c80f25017a3eaee7b
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-02-22 12:30:33 +0000

    extend and contract news component

--- linked-in-clone/src/components/feed/LinkedInNews.tsx
+++ linked-in-clone/src/components/feed/LinkedInNews.tsx
@@ -1,40 +1,81 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import {BsFillInfoSquareFill} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { BsFillInfoSquareFill, BsFillArrowDownCircleFill, BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 const LinkedInNews = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <Card>
-          <Card.Header>
-            <Card.Title>LinkedIn News<BsFillInfoSquareFill/></Card.Title>
-          </Card.Header>
-          <Card.Body>
-            <ul>
+      <Card.Title className='d-flex ml-4'>
+        LinkedIn News
+        <div className='ml-auto pr-4'>
+          <BsFillInfoSquareFill />
+        </div>
+      </Card.Title>
+      <Card.Body>
+        <ul>
+          <li>
+            <span>Why workers are 'conscious quitting'</span>
+            <p className='news-details'>9m ago &#8226; 20,998 readers</p>
+          </li>
+          <li>
+            <span>Gen Z workers are burnt out: report</span>
+            <p className='news-details'>10m ago &#8226; 14,799 readers</p>
+          </li>
+          <li>
+            <span>Honey, WFH shrunk the office</span>
+            <p className='news-details'>9m ago &#8226; 12,604 readers</p>
+          </li>
+          <li>
+            <span>'Breakthrough' for 4-day week trial</span>
+            <p className='news-details'>8m ago &#8226; 1,364 readers</p>
+          </li>
+          <li>
+            <span>How to decide between job offers</span>
+            <p className='news-details'>4d ago &#8226; 1,984 readers</p>
+          </li>
+          {showMore && (
+            <>
               <li>
-                <span>Why workers are 'conscious quitting'</span>
-                <span className="news-details">9m ago &#8226; 20,998 readers</span>
+                <span>How to avoid 'toxic positivity'</span>
+                <p className='news-details'>1d ago &#8226; 1,274 readers</p>
               </li>
               <li>
-                <span>Gen Z workers are burnt out: report</span>
-                <span className="news-details">10m ago &#8226; 14,799 readers</span>
+                <span>New quake strikes Türkiye, Syria</span>
+                <p className='news-details'>55m ago &#8226; 374 readers</p>
               </li>
               <li>
-                <span>Honey, WFH shrunk the office</span>
-                <span className="news-details">9m ago &#8226; 12,604 readers</span>
+                <span>Wall Street meets English football</span>
+                <p className='news-details'>53m ago &#8226; 2,340 readers</p>
               </li>
               <li>
-                <span>'Breakthrough' for 4-day week trial</span>
-                <span className="news-details">8m ago &#8226; 1,364 readers</span>
+                <span>Want a pay rise? Ask now, say experts</span>
+                <p className='news-details'>54m ago &#8226; 6,139 readers</p>
               </li>
               <li>
-                <span>How to decide between job offers</span>
-                <span className="news-details">4d ago &#8226; 1,984 readers</span>
+                <span>Contactless payments lead the charge</span>
+                <p className='news-details'>54m ago &#8226; 406 readers</p>
               </li>
-            </ul>
-          </Card.Body>
-        </Card>
-
+            </>
+          )}
+        </ul>
+        {showMore ? (
+          <Button variant='primary' onClick={handleClick}>
+            Show Less <BsFillArrowUpCircleFill/>
+          </Button>
+        ) : (
+          <Button variant='primary' onClick={handleClick}>
+            See More <BsFillArrowDownCircleFill/>
+          </Button>
+        )}
+      </Card.Body>
+    </Card>
   );
-}
+};
 
 export default LinkedInNews;
