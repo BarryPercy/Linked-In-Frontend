3ac1327b9c5cc24ccb0a251d33c8cd257846f8cd
Barry Percy <baz.percy@gmail.com>
2023-02-23 15:53:21 +0000

    Changes

--- linked-in-clone/src/components/feed/Posts.tsx
+++ linked-in-clone/src/components/feed/Posts.tsx
@@ -1,11 +1,15 @@
 import { fetchPosts} from "../../redux/actions";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { useEffect, useState } from "react";
-import { Card, Image, Button } from 'react-bootstrap'
+import { Card, Image, Button, Row, Col } from 'react-bootstrap'
 import parseISO from 'date-fns/parseISO'
 import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict'
+import { BsHandThumbsUp, BsFillHandThumbsUpFill } from 'react-icons/bs'
+import { MdOutlineInsertComment } from 'react-icons/md'
+import { BiRepost } from 'react-icons/bi'
+import { IoIosSend } from 'react-icons/io'
 
-interface PostsInterface{
+interface PostInterface{
     _id: string;
     text: string;
     username: string;
@@ -35,12 +39,23 @@ const Posts = () => {
     let postsReverse = [...posts].reverse();
     let slicedPostsReverse = postsReverse.slice(0,20);
 
+    
+
     useEffect(() => {
         dispatch(fetchPosts());
+        console.log(slicedPostsReverse)
       }, []);
     return (
         <div>
-            {slicedPostsReverse.map((post: PostsInterface) => {
+            {slicedPostsReverse.map((post: PostInterface) => {
+                let [liked, setLiked] = useState(false)
+                const toggleLiked = ()=>{
+                    if(liked){
+                        setLiked(false)
+                    }else{
+                        setLiked(true)
+                    }
+                }
                 return (
                     <Card key={post._id} className="posts">
                         <Card.Body className="ml-2 d-flex">
@@ -59,8 +74,26 @@ const Posts = () => {
                                         <p></p>
                                     </>
                                 </Card.Text>
-                            </div> 
+                            </div>
                         </Card.Body>
+                        <Card.Body>
+                            <hr/>
+                            <Row>
+                                <Col xs={3} className="justify-content-center d-flex comment-icon" onClick={toggleLiked}>
+                                    {liked?<div><BsFillHandThumbsUpFill className="align-self-center"/>Like</div>:<div><BsHandThumbsUp className="align-self-center"/>Like</div>}
+                                </Col>
+                                <Col xs={3} className="justify-content-center d-flex comment-icon">
+                                    <MdOutlineInsertComment className="align-self-center"/> Comment
+                                </Col>
+                                <Col xs={3} className="justify-content-center d-flex comment-icon">
+                                    <BiRepost className="align-self-center"/> Repost
+                                </Col>
+                                <Col xs={3} className="justify-content-center d-flex comment-icon">
+                                    <IoIosSend className="align-self-center"/> Send
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                        {/* <Card.Img src={post.image}/>  */}
                         
                     </Card>
                 );
