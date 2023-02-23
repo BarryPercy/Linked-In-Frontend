f582e391f60623cc9277de3990ba3ea7c61a839a
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-02-23 16:02:10 +0000

    .

--- linked-in-clone/src/components/feed/StartAPost.tsx
+++ linked-in-clone/src/components/feed/StartAPost.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { MdPhotoSizeSelectActual, MdOutlineArticle } from "react-icons/md";
-import { BsFillPlayBtnFill, BsBriefcaseFill } from "react-icons/bs";
+import {
+  BsFillPlayBtnFill,
+  BsBriefcaseFill,
+  BsEmojiSmile,
+  BsFillFileEarmarkTextFill,
+  BsThreeDots,
+} from "react-icons/bs";
+import { RxDividerVertical } from "react-icons/rx";
 import { Card, Image, Button, Modal } from "react-bootstrap";
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { useAppDispatch } from "../../redux/hooks";
-import { postImage, postPost } from "../../redux/actions";
+import { postPost } from "../../redux/actions";
+import {} from "react-icons/bs";
 
 export default function StartAPost() {
-  const [image, setImage] =useState<File | null>(null);
   const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
   const [post, setPost] = useState("");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -18,16 +26,15 @@ export default function StartAPost() {
     const object = {
       text: "",
     };
-
-    const object2 = {
-      post: image
-    };
     object.text += post;
-    console.log(object)
-    console.log(object2)
     dispatch(postPost(object));
-    dispatch(postImage(object2))
     handleClose();
+  };
+
+  //  This is for the upload modal
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => {
+    setShow2(true);
   };
 
   return (
@@ -60,39 +67,54 @@ export default function StartAPost() {
                   <Modal.Title>Create a Post</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-  <div className="d-flex my-2">
-    <Image className="avatar" src="./images/jovelynn.png" />
-    <h5 className="align-self-center">Jovellyn Quiapos</h5>
-  </div>
-  <Form.Control
-    as="textarea"
-    placeholder="What do you want to talk about?"
-    style={{ height: "150px" }}
-    onChange={(e) => {
-      setPost(e.target.value);
-    }}
-    value={post}
-  />
-  {image && (
-    <div className="my-2">
-      <Image src={URL.createObjectURL(image)} thumbnail />
-    </div>
-  )}
-  <Form.Group>
-    <Form.Label>Choose an image to upload:</Form.Label>
-    <Form.Control
-      type="file"
-      accept="image/*"
-      onChange={(e) => setImage((e.target as HTMLInputElement)?.files?.[0] ?? null)}
-    />
-  </Form.Group>
-</Modal.Body>
+                  <div className="d-flex my-2">
+                    <Image className="avatar" src="./images/jovelynn.png" />
+                    <h5 className="align-self-center">Jovellyn Quiapos</h5>
+                  </div>
+                  <Form.Control
+                    className="post-area"
+                    as="textarea"
+                    placeholder="What do you want to talk about?"
+                    style={{ height: "150px" }}
+                    onChange={(e) => {
+                      setPost(e.target.value);
+                    }}
+                    value={post}
+                  />
+                </Modal.Body>
+                <div className="ml-3 mb-3 icon-posts">
+                  <BsEmojiSmile style={{ fontSize: "25px" }} />
+                </div>
+                <div className="d-flex">
+                  <div className="d-flex side-post-icons">
+                    <Button
+                      className="img-icon-btn ml-3 mb-2"
+                      onClick={() => {
+                        handleClose();
+                        handleShow2();
+                      }}
+                    >
+                      <div>
+                        <MdPhotoSizeSelectActual style={{ fontSize: "25px" }} />
+                      </div>
+                    </Button>
 
-                <Modal.Footer>
-                  <Button variant="primary" onClick={handleSubmit}>
-                    Post
-                  </Button>
-                </Modal.Footer>
+                    <div className=" mb-2 icon-posts">
+                      <BsFillPlayBtnFill style={{ fontSize: "25px" }} />
+                    </div>
+                    <div className=" mb-2 icon-posts">
+                      <BsFillFileEarmarkTextFill style={{ fontSize: "25px" }} />
+                    </div>
+                    <div className=" mb-2 icon-posts">
+                      <BsThreeDots style={{ fontSize: "25px" }} />
+                    </div>
+                  </div>
+                  <div className="right-side-icon ml-auto mr-3">
+                    <Button variant="primary" onClick={handleSubmit}>
+                      Post
+                    </Button>
+                  </div>
+                </div>
               </Modal>
             </div>
           </div>
@@ -100,14 +122,48 @@ export default function StartAPost() {
             id="start-a-post-lower"
             className="small-header-text d-flex justify-content-around"
           >
-            <div className="start-a-post-icon-text gray-hover d-flex align-items-center">
-              <MdPhotoSizeSelectActual
-                className="text-primary"
-                style={{ fontSize: "20px" }}
-              />
+            <Button
+              id="start-a-picture"
+              className="start-a-picture-button d-flex  ml-3 p-1"
+              onClick={handleShow2}
+            >
+              <div className="start-a-post-icon-text d-flex align-items-center">
+                <MdPhotoSizeSelectActual
+                  className="text-primary"
+                  style={{ fontSize: "20px" }}
+                />
 
-              <span className="pl-2">Photo</span>
-            </div>
+                <span className="pl-2">Photo</span>
+              </div>
+            </Button>
+            <Modal show={show2} onHide={handleClose2} size="lg">
+              <Modal.Header closeButton>
+                <Modal.Title>Upload Image</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {/* <div className="d-flex my-2">
+                  <Image className="avatar" src="./images/jovelynn.png" />
+                  <h5 className="align-self-center">Jovellyn Quiapos</h5>
+                </div> */}
+                <div className="upload-area">
+                  <Button id="share-a-img" className="share-a-img-btn">
+                    Select image to share
+                  </Button>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button
+                  className="cancel-img-btn"
+                  variant="primary"
+                  onClick={handleClose2}
+                >
+                  Cancel
+                </Button>
+                <Button className="upload-img-btn" variant="secondary">
+                  Done
+                </Button>
+              </Modal.Footer>
+            </Modal>
             <div className="start-a-post-icon-text gray-hover d-flex align-items-center ">
               <BsFillPlayBtnFill
                 className="text-success"
