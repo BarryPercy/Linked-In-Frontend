2b33798d26557e1ac1102fa8df5b0b4be3f4d6d5
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-02-23 13:11:35 +0000

    Modal for image feed css done

--- linked-in-clone/src/components/feed/StartAPost.tsx
+++ linked-in-clone/src/components/feed/StartAPost.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { MdPhotoSizeSelectActual, MdOutlineArticle } from "react-icons/md";
-import { BsFillPlayBtnFill, BsBriefcaseFill } from "react-icons/bs";
+import {
+  BsFillPlayBtnFill,
+  BsBriefcaseFill,
+  BsEmojiSmile,
+  BsFillFileEarmarkTextFill,
+  BsThreeDots,
+} from "react-icons/bs";
+import { RxDividerVertical } from "react-icons/rx";
 import { Card, Image, Button, Modal } from "react-bootstrap";
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { useAppDispatch } from "../../redux/hooks";
 import { postPost } from "../../redux/actions";
+import {} from "react-icons/bs";
 
 export default function StartAPost() {
   const [show, setShow] = useState(false);
@@ -64,6 +72,7 @@ export default function StartAPost() {
                     <h5 className="align-self-center">Jovellyn Quiapos</h5>
                   </div>
                   <Form.Control
+                    className="post-area"
                     as="textarea"
                     placeholder="What do you want to talk about?"
                     style={{ height: "150px" }}
@@ -73,11 +82,39 @@ export default function StartAPost() {
                     value={post}
                   />
                 </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="primary" onClick={handleSubmit}>
-                    Post
-                  </Button>
-                </Modal.Footer>
+                <div className="ml-3 mb-3 icon-posts">
+                  <BsEmojiSmile style={{ fontSize: "25px" }} />
+                </div>
+                <div className="d-flex">
+                  <div className="d-flex side-post-icons">
+                    <Button
+                      className="img-icon-btn ml-3 mb-2"
+                      onClick={() => {
+                        handleClose();
+                        handleShow2();
+                      }}
+                    >
+                      <div>
+                        <MdPhotoSizeSelectActual style={{ fontSize: "25px" }} />
+                      </div>
+                    </Button>
+
+                    <div className=" mb-2 icon-posts">
+                      <BsFillPlayBtnFill style={{ fontSize: "25px" }} />
+                    </div>
+                    <div className=" mb-2 icon-posts">
+                      <BsFillFileEarmarkTextFill style={{ fontSize: "25px" }} />
+                    </div>
+                    <div className=" mb-2 icon-posts">
+                      <BsThreeDots style={{ fontSize: "25px" }} />
+                    </div>
+                  </div>
+                  <div className="right-side-icon ml-auto mr-3">
+                    <Button variant="primary" onClick={handleSubmit}>
+                      Post
+                    </Button>
+                  </div>
+                </div>
               </Modal>
             </div>
           </div>
