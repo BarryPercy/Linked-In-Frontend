e4b455044dfa49a8be35e6697ac59e173c40c9eb
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-02-24 15:51:10 +0000

    DONE!!!!!!!

--- linked-in-clone/src/components/SecondNav.tsx
+++ linked-in-clone/src/components/SecondNav.tsx
@@ -1,40 +1,62 @@
-import React, { useEffect } from 'react';
-import { Button, Image } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import { Button, Image } from "react-bootstrap";
 import userPic from "../images/jovelynn.png";
+import { useAppSelector } from "../redux/hooks";
 
 function SecondNav() {
-
+  let currentUser = useAppSelector((state) => state.users.currentUser);
   function handleScroll() {
     const distance = window.scrollY;
-  const nav = document.querySelector('.secondary-nav');
+    const nav = document.querySelector(".secondary-nav");
 
-  if (nav) {
-    if (distance > 50) { 
-      nav.classList.add('show-nav');
-    } else {
-      nav.classList.remove('show-nav');
+    if (nav) {
+      if (distance > 450) {
+        nav.classList.add("show-nav");
+      } else {
+        nav.classList.remove("show-nav");
+      }
     }
   }
-}
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className='secondary-nav d-flex align-items-center justify-content-around fixed-nav'>
-      <div className='d-flex align-items-center'>
-        <Image src={userPic} alt="Profile" roundedCircle style={{width: 36, height: 36}} />
-        <div className='ml-3'>
-          <p style={{fontSize: "15px", marginBottom: "0", marginTop: "10px"}}>Jovellyn A Quiapos</p>
-          <p style={{fontSize: "10px"}}>Jr Full Stack Dev</p>
+    <div className="secondary-nav d-flex align-items-center justify-content-around fixed-nav">
+      <div className="d-flex align-items-center">
+        <Image
+          src={currentUser.image}
+          alt="Profile"
+          roundedCircle
+          style={{ width: 36, height: 36 }}
+        />
+        <div className="ml-3">
+          <p style={{ fontSize: "15px", marginBottom: "0", marginTop: "10px" }}>
+            {currentUser.name} {currentUser.surname}
+          </p>
+          <p style={{ fontSize: "10px" }}>{currentUser.title}</p>
         </div>
       </div>
-      <div className='button-area'>
-        <Button className='mx-1 more-btn' variant="outline-secondary" href="#">More</Button>
-        <Button className='mx-1 add-btn' variant="outline-primary" href="#">Edit Profile</Button>
-        <Button className='mx-1 open-btn' variant="primary" href="#">Open to</Button>
+      <div className="button-area">
+        <Button
+          className="mx-1 py-0 more-btn"
+          variant="outline-secondary"
+          href="#"
+        >
+          More
+        </Button>
+        <Button
+          className="mx-1 py-0 add-btn"
+          variant="outline-primary"
+          href="#"
+        >
+          Edit Profile
+        </Button>
+        <Button className="mx-1 py-0 open-btn" variant="primary" href="#">
+          Open to
+        </Button>
       </div>
     </div>
   );
