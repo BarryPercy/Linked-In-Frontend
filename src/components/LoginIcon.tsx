89f736c591c7c267393ca37eaa1ccc2d296f43f4
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-03-23 16:23:37 +0100

    loginpage has 3 profiles with <a>

--- src/components/LoginIcon.tsx
+++ src/components/LoginIcon.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 interface User {
   _id: string;
@@ -18,21 +19,21 @@ const LoginIcon = (props: User & { children?: React.ReactNode }) => {
   console.log(props);
 
   return (
-    <div>
+    <a href="/">
       <Card className="profileLoginIcon">
         <Card.Img
           className="profileImage mt-3"
           variant="top"
-          src="https://i.pinimg.com/474x/6b/02/e1/6b02e1d3980ef94b6fdbd21abfb78b27.jpg"
+          src={props.image}
         />
         <Card.Body>
           <Card.Title className="pt-1 text-white font-weight-bold">
-            Name <br />
-            Surname
+            {props.name} <br />
+            {props.surname}
           </Card.Title>
         </Card.Body>
       </Card>
-    </div>
+    </a>
   );
 };
 
